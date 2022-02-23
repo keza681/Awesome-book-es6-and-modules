8ec22edac72c13c3cd9a0966fce2e8e7a05c99e1
keza681 <lindakeza19@gmail.com>
2022-02-23 18:02:33 +0200

    Describe readmefile

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line no-unused-vars
 import * as main from './modules/script.js';
 import * as inter from './modules/medium.js';
 import * as sections from './modules/sectionFolder.js';
